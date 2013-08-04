4eda58edc3bf6f9719f0e8c5fbdcb03d9edc0bee
Xu Lei <dogman.xu@gmail.com>
2013-08-04 21:31:04 +0300

    changes in appViewModel

--- scripts/appViewModel.js
+++ scripts/appViewModel.js
@@ -1,18 +1,19 @@
 define(["knockout"], function (ko) {
     return function () {
-        this.firstName = ko.observable("Bert");
-        this.firstNameCaps = ko.computed(function () {
-            return this.firstName().toUpperCase()
-        }, this);
-        this.names = ko.observableArray([]);
-        this.add = function (ko) {
-            this.names.push(ko);
+        var ori = this;
+        ori.firstName = ko.observable("Bert");
+        ori.firstNameCaps = ko.computed(function () {
+            return ori.firstName().toUpperCase();
+        }, ori);
+        ori.names = ko.observableArray([]);
+        ori.add = function (ko) {
+            ori.names.push(ko);
         };
-        this.remove = function () {
-            this.names.remove(this);
+        ori.remove = function () {
+            ori.names.remove(this);
         };
-        this.count = ko.computed(function () {
-            return this.names().length;
-        }, this);
-    }
+        ori.count = ko.computed(function () {
+            return ori.names().length;
+        }, ori);
+    };
 });
