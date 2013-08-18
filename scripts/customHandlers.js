b111e70f4a5dc7de86ba5666c49d2a6ef1fd2a70
xlei <xlei@ubuntu.(none)>
2013-08-18 21:51:30 +0300

    fix cancel button

--- scripts/customHandlers.js
+++ scripts/customHandlers.js
@@ -2,9 +2,12 @@ define(["knockout", "jquery"], function (ko, $) {
     
     ko.bindingHandlers.editableRow = {
         init: function (element, valueAccessor) {
+            var oriName;
+            
             $(element).find(".editRow").hide().end().find(".detailRow").show();
             
             $(element).find(".editBtn").on("click", function () {
+            	oriName = valueAccessor().name();
                 $(element).find(".editRow, .detailRow").toggle();
             });
             
@@ -13,6 +16,7 @@ define(["knockout", "jquery"], function (ko, $) {
             });
             
             $(element).find(".cancelBtn").on("click", function () {
+            	valueAccessor().name(oriName);
                 $(element).find(".editRow, .detailRow").toggle();
             });
         },
